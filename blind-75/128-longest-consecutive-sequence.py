89b93035996a1f80ff03c17a72e036c2a636fa4e
pierre <brad.knp@gmail.com>
2024-04-26 08:42:12 -0400

    128 completed

--- blind-75/128-longest-consecutive-sequence.py
+++ blind-75/128-longest-consecutive-sequence.py
@@ -1,3 +1,16 @@
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        
+    def longestConsecutive(self, nums: list[int]) -> int:
+        longest = 0
+        myset = set(nums)
+        for num in nums:
+            if num -1 not in myset:
+                length = 0
+                while (num+ length) in myset:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+    
+sol = Solution()
+
+print(sol.longestConsecutive(nums = [100,4,200,1,3,2]))
+                
