5947196af380eaadf49dabd96ac89ecd990fb721
pierre <brad.knp@gmail.com>
2024-04-27 15:33:15 -0400

    done with alien dict

--- blind-75/269-alien-dictionary.py
+++ blind-75/269-alien-dictionary.py
@@ -11,5 +11,31 @@ class Solution:
             
             for j in range(minlen):
                 if w1[j] != w2[j]:
-                    adj[w1[j]].add(w2(j))
+                    adj[w1[j]].add(w2[j])
                     break
+        visit = {}
+        res = []
+        
+        
+        def dfs(c):
+            if c in visit:
+                return visit[c]
+            
+            visit[c] = True
+            
+            for nei in adj[c]:
+                if dfs(nei):
+                    return True
+                
+            visit[c] = False
+            res.append(c)
+            
+        for c in adj:
+            if dfs(c):
+                return ""
+        res.reverse()
+        return "".join(res)
+        
+sol = Solution()
+
+print(sol.alienOrder(['wee', 'wrt', 'opl']))
