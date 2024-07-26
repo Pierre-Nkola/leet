36e87625f16794138703c8c222fbdbd7ce925863
pierre <brad.knp@gmail.com>
2024-07-26 08:20:15 -0400

    done learning how to find k closest element

---
+++ blind-75-practice/973-k-closest-points-to-origin.py
@@ -0,0 +1,19 @@
+import heapq
+from typing import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        max_heap = []
+        
+        for x,y in points:
+            distance = -(x**2 + y**2)
+            if len(max_heap) < k:
+                heapq.heappush(max_heap, (distance, x,y))
+            else:
+                heapq.heappushpop(max_heap, (distance, x, y))
+        return [[x,y] for i, x,y in max_heap]
+# Example usage:
+sol = Solution()
+points = [[1, 3], [-2, 2], [5, 8], [0, 1]]
+k = 2
+print(sol.kClosest(points, k))
