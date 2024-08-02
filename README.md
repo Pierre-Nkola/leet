d32a6e7242ab2af512bf4f91826f8c598aa7fe00
pierre <brad.knp@gmail.com>
2024-08-02 14:23:51 -0400

    backtracking problem with high time complexity. the base case is the length of the string and the recursive call happens within the loop going over the elements.

---
+++ README.md
@@ -0,0 +1,11 @@
+# leet 
+
+This repo was inspired by a list published by a renowned Tech engineer on Blind, an anonimous app where tech workers(mostly) express their mind. The initial idea suggested that mastering those 75 problems is enough to crack any software engineer coding interview.
+
+This repo is a testimony of me trying.
+
+Areas where I need improvements:
+1- Dynamic Programming
+2- DFS
+3- BFS
+4- backtracking
---
+++ blind-75-practice/17-letter-combination-of-phone-number.py
@@ -0,0 +1,31 @@
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        wordtoDict = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+        }
+        res = []
+        def backtrack(i, curString):
+            if len(curString) == len(digits):
+                res.append(curString)
+                return
+            
+            for c in wordtoDict[digits[i]]:
+                backtrack(i + 1, curString + c )
+                
+        if digits:
+            backtrack(0,"")
+            
+        return res
+
+sol = Solution()
+
+print(sol.letterCombinations(digits = "23"))
+        
